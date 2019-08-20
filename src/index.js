73ec7a45414c34ed679d4c3a4e71775eb0bbd5d6
Ramiro González Maciel <ramirogm@gmail.com>
2019-08-20 11:29:48 -0300

    Move package.json to root of the package to be able to import the SDK from other projects via a github dependency

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ const Cache = require('@internal/cache');
 const inboundApi = new Koa();
 const outboundApi = new Koa();
 
-const inboundApiSpec = yaml.load('./inboundApi/api.yaml');
-const outboundApiSpec = yaml.load('./outboundApi/api.yaml');
+const inboundApiSpec = yaml.load('./src/inboundApi/api.yaml');
+const outboundApiSpec = yaml.load('./src/outboundApi/api.yaml');
 
 const Jws = require('@modusbox/mojaloop-sdk-standard-components').Jws;
 const Errors = require('@modusbox/mojaloop-sdk-standard-components').Errors;
