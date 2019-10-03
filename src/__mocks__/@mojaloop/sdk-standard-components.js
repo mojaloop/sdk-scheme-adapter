cea70a0306c0e857ec547b5dcd86f4e0b6cd916f
James Bush <james.bush@modusbox.com>
2019-10-03 14:32:31 +0100

    add extension list capability for quote and transfer outbound model

--- src/__mocks__/@mojaloop/sdk-standard-components.js
+++ src/__mocks__/@mojaloop/sdk-standard-components.js
@@ -22,19 +22,19 @@ class MockMojaloopRequests extends EventEmitter {
     }
 
     getParties(...args) {
-        console.log(`MockMojaloopRequests.getParties called with args: ${util.inspect(args)}`);
+        console.log(`MockMojaloopRequests.getParties called with args: ${util.inspect(args, { depth: 20 })}`);
         setImmediate(() => { this.emit('getParties'); });
         return Promise.resolve(null);
     }
 
     postQuotes(...args) {
-        console.log(`MockMojaloopRequests.postQuotes called with args: ${util.inspect(args)}`);
+        console.log(`MockMojaloopRequests.postQuotes called with args: ${util.inspect(args, { depth: 20 })}`);
         setImmediate(() => { this.emit('postQuotes'); });
         return Promise.resolve(null);
     }
 
     postTransfers(...args) {
-        console.log(`MockMojaloopRequests.postTransfers called with args: ${util.inspect(args)}`);
+        console.log(`MockMojaloopRequests.postTransfers called with args: ${util.inspect(args, { depth: 20 })}`);
         setImmediate(() => { this.emit('postTransfers'); });
         return Promise.resolve(null);
     }
