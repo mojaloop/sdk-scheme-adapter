93876d3f8c0f81e3d8f6ecd13da2d68945c32597
Murthy Kakarlamudi <murthy@modusbox.com>
2020-01-27 22:19:39 -0500

    Updates for request to pay

--- src/test/unit/outboundApi/handlers.test.js
+++ src/test/unit/outboundApi/handlers.test.js
@@ -11,12 +11,14 @@
 'use strict';
 
 const mockError = require('./data/mockError');
+const mockRequestToPayError = require('./data/mockRequestToPayError');
 const transferRequest = require('./data/transferRequest');
+const requestToPayPayload = require('./data/requestToPay');
 
 jest.mock('@internal/model');
 
 const handlers = require('../../../OutboundServer/handlers');
-const { OutboundTransfersModel } = require('@internal/model');
+const { OutboundTransfersModel,OutboundRequestToPayModel } = require('@internal/model');
 
 /**
  * Mock the outbound transfer model to simulate throwing errors
@@ -26,6 +28,26 @@ OutboundTransfersModel.mockImplementation(() => {
         run: async () => {
             // throw the mockError object when the model is run
             throw mockError;
+        },
+        initialize: async () => {
+            // nothing needed here
+            return;
+        },
+        load: async () => {
+            // nothing needed here
+            return;
+        }
+    };
+});
+
+/**
+ * Mock the outbound request to pay model to simulate throwing errors
+ */
+OutboundRequestToPayModel.mockImplementation(() => {
+    return {
+        run: async () => {
+            // throw the mockError object when the model is run
+            throw mockRequestToPayError;
         },
         initialize: async () => {
             // nothing needed here
@@ -99,6 +121,33 @@ describe('Outbound API handlers:', () => {
             expect(mockContext.response.body.message).toEqual('Mock error');
             expect(mockContext.response.body.statusCode).toEqual('3204');
             expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
+        });
+    });
+
+    describe('POST /requestToPay', () => {
+        test('returns correct error response body when model throws mojaloop error', async () => {
+            const mockContext = {
+                request: {
+                    body: requestToPayPayload,
+                    headers: {
+                        'fspiop-source': 'foo'
+                    }
+                },
+                response: {},
+                state: {
+                    conf: {},
+                    logger: console
+                }
+            };
+
+            await handlers['/requestToPay'].post(mockContext);
+
+            // check response is correct
+            expect(mockContext.response.status).toEqual(500);
+            expect(mockContext.response.body).toBeTruthy();
+            expect(mockContext.response.body.message).toEqual('Mock error');
+            expect(mockContext.response.body.statusCode).toEqual('3204');
+            expect(mockContext.response.body.requestToPayState).toEqual(mockRequestToPayError.requestToPayState);
         });
     });
 
