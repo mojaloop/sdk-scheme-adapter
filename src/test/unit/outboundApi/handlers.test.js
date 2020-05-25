781014793d23cab64149bb0abd759b06a4ea202a
Steven Oderayi <oderayi@gmail.com>
2020-05-25 11:45:04 +0100

    Add tests for outbound API. Fix missing specs entries.

--- src/test/unit/outboundApi/handlers.test.js
+++ src/test/unit/outboundApi/handlers.test.js
@@ -14,18 +14,66 @@ const mockError = require('./data/mockError');
 const mockRequestToPayError = require('./data/mockRequestToPayError');
 const mockRequestToPayTransferError = require('./data/mockRequestToPayTransferError');
 const transferRequest = require('./data/transferRequest');
+const bulkTransferRequest = require('./data/bulkTransferRequest.json');
+const bulkQuoteRequest = require('./data/bulkQuoteRequest.json');
 const requestToPayPayload = require('./data/requestToPay');
 const requestToPayTransferRequest = require('./data/requestToPayTransferRequest');
 
 jest.mock('@internal/model');
 
 const handlers = require('../../../OutboundServer/handlers');
-const { OutboundTransfersModel, OutboundRequestToPayTransferModel, OutboundRequestToPayModel } = require('@internal/model');
+const {
+    OutboundTransfersModel,
+    OutboundBulkTransfersModel,
+    OutboundBulkQuotesModel,
+    OutboundRequestToPayTransferModel,
+    OutboundRequestToPayModel,
+} = require('@internal/model');
 
 /**
  * Mock the outbound transfer model to simulate throwing errors
  */
 OutboundTransfersModel.mockImplementation(() => {
+    return {
+        run: async () => {
+            // throw the mockError object when the model is run
+            throw mockError;
+        },
+        initialize: async () => {
+            // nothing needed here
+            return;
+        },
+        load: async () => {
+            // nothing needed here
+            return;
+        }
+    };
+});
+
+/**
+ * Mock the outbound bulk transfers model to simulate throwing errors
+ */
+OutboundBulkTransfersModel.mockImplementation(() => {
+    return {
+        run: async () => {
+            // throw the mockError object when the model is run
+            throw mockError;
+        },
+        initialize: async () => {
+            // nothing needed here
+            return;
+        },
+        load: async () => {
+            // nothing needed here
+            return;
+        }
+    };
+});
+
+/**
+ * Mock the outbound bulk quotes model to simulate throwing errors
+ */
+OutboundBulkQuotesModel.mockImplementation(() => {
     return {
         run: async () => {
             // throw the mockError object when the model is run
@@ -82,9 +130,158 @@ OutboundRequestToPayModel.mockImplementation(() => {
     };
 });
 
-
 describe('Outbound API handlers:', () => {
     describe('POST /transfers', () => {
+        test('returns correct error response body when model throws mojaloop error', async () => {
+            const mockContext = {
+                request: {
+                    body: transferRequest,
+                    headers: {
+                        'fspiop-source': 'foo'
+                    }
+                },
+                response: {},
+                state: {
+                    conf: {},
+                    logger: console
+                }
+            };
+
+            await handlers['/transfers'].post(mockContext);
+            
+            // check response is correct
+            expect(mockContext.response.status).toEqual(500);
+            expect(mockContext.response.body).toBeTruthy();
+            expect(mockContext.response.body.message).toEqual('Mock error');
+            expect(mockContext.response.body.statusCode)
+                .toEqual(mockError.transferState.lastError.mojaloopError.errorInformation.errorCode);
+            expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
+        });
+
+        test('uses correct extension list error code for response body statusCode when configured to do so', async () => {
+            const mockContext = {
+                request: {
+                    body: transferRequest,
+                    headers: {
+                        'fspiop-source': 'foo'
+                    }
+                },
+                response: {},
+                state: {
+                    conf: {
+                        outboundErrorStatusCodeExtensionKey: 'extErrorKey'  // <- tell the handler to use this extensionList item as source of statusCode
+                    },
+                    logger: console
+                }
+            };
+
+            await handlers['/transfers'].post(mockContext);
+
+            // check response is correct
+            expect(mockContext.response.status).toEqual(500);
+            expect(mockContext.response.body).toBeTruthy();
+            expect(mockContext.response.body.message).toEqual('Mock error');
+
+            // in this case, where we have set outboundErrorExtensionKey config we expect the error body statusCode
+            // property to come from the extensionList item with the corresponding key 'extErrorKey'
+            expect(mockContext.response.body.statusCode).toEqual('9999');
+            expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
+        });
+    });
+
+    describe('PUT /transfers', () => {
+        test('returns correct error response body when model throws mojaloop error', async () => {
+            const mockContext = {
+                request: {
+                    body: {
+                        acceptQuote: true
+                    },
+                    headers: {
+                        'fspiop-source': 'foo'
+                    }
+                },
+                response: {},
+                state: {
+                    conf: {},
+                    logger: console,
+                    path: {
+                        params: {
+                            transferId: '12345'
+                        }
+                    }
+                }
+            };
+
+            await handlers['/transfers/{transferId}'].put(mockContext);
+
+            // check response is correct
+            expect(mockContext.response.status).toEqual(500);
+            expect(mockContext.response.body).toBeTruthy();
+            expect(mockContext.response.body.message).toEqual('Mock error');
+            expect(mockContext.response.body.statusCode).toEqual('3204');
+            expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
+        });
+    });
+
+    describe('POST /bulkTransfers', () => {
+        test('returns correct error response body when model throws mojaloop error', async () => {
+            const mockContext = {
+                request: {
+                    body: transferRequest,
+                    headers: {
+                        'fspiop-source': 'foo'
+                    }
+                },
+                response: {},
+                state: {
+                    conf: {},
+                    logger: console
+                }
+            };
+
+            await handlers['/transfers'].post(mockContext);
+            
+            // check response is correct
+            expect(mockContext.response.status).toEqual(500);
+            expect(mockContext.response.body).toBeTruthy();
+            expect(mockContext.response.body.message).toEqual('Mock error');
+            expect(mockContext.response.body.statusCode)
+                .toEqual(mockError.transferState.lastError.mojaloopError.errorInformation.errorCode);
+            expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
+        });
+
+        test('uses correct extension list error code for response body statusCode when configured to do so', async () => {
+            const mockContext = {
+                request: {
+                    body: transferRequest,
+                    headers: {
+                        'fspiop-source': 'foo'
+                    }
+                },
+                response: {},
+                state: {
+                    conf: {
+                        outboundErrorStatusCodeExtensionKey: 'extErrorKey'  // <- tell the handler to use this extensionList item as source of statusCode
+                    },
+                    logger: console
+                }
+            };
+
+            await handlers['/transfers'].post(mockContext);
+
+            // check response is correct
+            expect(mockContext.response.status).toEqual(500);
+            expect(mockContext.response.body).toBeTruthy();
+            expect(mockContext.response.body.message).toEqual('Mock error');
+
+            // in this case, where we have set outboundErrorExtensionKey config we expect the error body statusCode
+            // property to come from the extensionList item with the corresponding key 'extErrorKey'
+            expect(mockContext.response.body.statusCode).toEqual('9999');
+            expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
+        });
+    });
+
+    describe('POST /bulkQuotes', () => {
         test('returns correct error response body when model throws mojaloop error', async () => {
             const mockContext = {
                 request: {
@@ -169,41 +366,6 @@ describe('Outbound API handlers:', () => {
         });
     });
 
-
-    describe('PUT /transfers', () => {
-        test('returns correct error response body when model throws mojaloop error', async () => {
-            const mockContext = {
-                request: {
-                    body: {
-                        acceptQuote: true
-                    },
-                    headers: {
-                        'fspiop-source': 'foo'
-                    }
-                },
-                response: {},
-                state: {
-                    conf: {},
-                    logger: console,
-                    path: {
-                        params: {
-                            transferId: '12345'
-                        }
-                    }
-                }
-            };
-
-            await handlers['/transfers/{transferId}'].put(mockContext);
-
-            // check response is correct
-            expect(mockContext.response.status).toEqual(500);
-            expect(mockContext.response.body).toBeTruthy();
-            expect(mockContext.response.body.message).toEqual('Mock error');
-            expect(mockContext.response.body.statusCode).toEqual('3204');
-            expect(mockContext.response.body.transferState).toEqual(mockError.transferState);
-        });
-    });
-
     describe('POST /requestToPay', () => {
         test('returns correct error response body when model throws mojaloop error', async () => {
             const mockContext = {
@@ -230,5 +392,4 @@ describe('Outbound API handlers:', () => {
             expect(mockContext.response.body.requestToPayState).toEqual(mockRequestToPayError.requestToPayState);
         });
     });
-
 });
