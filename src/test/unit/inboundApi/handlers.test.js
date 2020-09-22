a4a66e7882a5ccdb4e8ca7b6015e118c4d3e3196
Valentin <valentin.genev@modusbox.com>
2020-09-23 01:09:18 +0300

    reverted accent character commit

--- src/test/unit/inboundApi/handlers.test.js
+++ src/test/unit/inboundApi/handlers.test.js
@@ -39,7 +39,6 @@ describe('Inbound API handlers:', () => {
         let mockContext;
 
         beforeEach(() => {
-            jest.clearAllMocks();
             mockContext = {
                 request: {
                     body: mockArgs.quoteRequest,
@@ -57,17 +56,6 @@ describe('Inbound API handlers:', () => {
         });
 
         test('calls `model.quoteRequest` with the expected arguments.', async () => {
-            const quoteRequestSpy = jest.spyOn(Model.prototype, 'quoteRequest');
-
-            await expect(handlers['/quotes'].post(mockContext)).resolves.toBe(undefined);
-
-            expect(quoteRequestSpy).toHaveBeenCalledTimes(1);
-            expect(quoteRequestSpy.mock.calls[0][0]).toBe(mockContext.request.body);
-            expect(quoteRequestSpy.mock.calls[0][1]).toBe(mockContext.request.headers['fspiop-source']);
-        });
-
-        test('calls `model.quoteRequest` with accented characters in names', async () => {
-            mockContext.request.body = mockArgs.quoteRequestAccented;
             const quoteRequestSpy = jest.spyOn(Model.prototype, 'quoteRequest');
 
             await expect(handlers['/quotes'].post(mockContext)).resolves.toBe(undefined);
