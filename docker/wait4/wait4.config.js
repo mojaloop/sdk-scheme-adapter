df655675c2bed46ebf60ecd880e4e2ab85377210
Paweł Marzec <pm@eoln.eu>
2021-02-10 11:10:21 +0100

    ci: add wait4 script before runing integration test

---
+++ docker/wait4/wait4.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  // format version sem-ver
+  // `v{major}.${minor}.${patch}`
+  wait4: 'v0.1.0',
+
+  // How many times should we retry waiting for a service?
+  retries: 60,
+
+  // How many ms to wait before retrying a service connection?
+  waitMs: 2500,
+
+  // services definitions
+  services: [
+    {
+      name: 'sdk-scheme-adapter',
+
+      // list of services to wait for
+      wait4: [
+        {
+          description: 'Redis Cache',
+          uri: 'redis:6379',
+          method: 'ncat'
+        }
+      ]
+    },
+    {
+      name: 'scheme-adapter-int',
+      wait4: [
+        {
+          description: 'Inbound service',
+          uri: 'scheme-adapter:4000',
+          method: 'ncat'
+        },
+        {
+          description: 'Outbound service',
+          uri: 'scheme-adapter:4001',
+          method: 'ncat'
+        },
+        {
+          uri: 'ml-testing-toolkit:5000',
+          method: 'ncat'
+        },
+      ]
+    },
+    {
+      name: 'ml-testing-toolkit',
+      wait4: [
+        {
+          description: 'Inbound service',
+          uri: 'scheme-adapter:4000',
+          method: 'ncat'
+        },
+        {
+          description: 'MongoDB object store',
+          uri: 'mongodb://mongo:27018/dfsps',
+          method: 'mongo'
+        }
+      ]
+    }
+  ]
+}
