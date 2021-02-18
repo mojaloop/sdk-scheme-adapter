10b4fc045afb7c1ed08685eaf3656de009a651be
Paweł Marzec <2881004+eoln@users.noreply.github.com>
2021-02-18 12:39:47 +0100

    feat: 1993 sync post /quotes @Outgoing (#251)
    * chore: bring pr-title back
    
    * feat: QuotesModel generated
    
    * test: first draft of QuotesModel unit tests
    
    * feat: reformatMessage method for Async2Sync
    
    * feat: implement PUT /quotes @ inbound & tests
    
    * feat: /quotes handler for Outbound service
    
    * feat: add cors middleware for outbound
    
    * feat: propagate config to mojaloop requests
    
    * test: integration for POST /quotes
    
    * feat: POST /quotes payload to be conformant with mojaloop API
    
    * refactor: api for /quotes
    
    * fix: use api-snippets 12.0.3

---
+++ src/test/integration/lib/Outbound/quotes.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const axios = require('axios');
+const { uuid } = require('uuidv4');
+const env = require('../../testEnv');
+const quotesPostRequest = require('./data/quotesPostRequest.json');
+
+jest.dontMock('redis');
+
+describe('/quotes', () => {
+
+    test('post - happy flow', async () => {
+        const postQuotesURI = `${env.OutboundHostURI}/quotes`;
+        const quoteId = uuid();
+        const res = await axios({
+            method: 'POST',
+            url: postQuotesURI,
+            data: {
+                fspId: 'switch',
+                quotesPostRequest: {
+                    ...quotesPostRequest,
+                    quoteId
+                }
+            },
+            headers: {
+                'access-control-allow-origin': '*'
+            }
+        });
+
+        expect(res.status).toEqual(200);
+        expect(res.data.currentState).toEqual('COMPLETED');
+        expect(typeof res.data.quotes).toEqual('object');
+    });
+
+    test('post - timeout', (done) => {
+        const postQuotesURI = `${env.OutboundHostURI}/quotes`;
+        const quoteId = uuid();
+        axios({
+            method: 'POST',
+            url: postQuotesURI,
+            data: {
+                fspId: 'timeout-fsp-id',
+                quotesPostRequest: {
+                    ...quotesPostRequest,
+                    quoteId
+                }
+            },
+            headers: {
+                'access-control-allow-origin': '*'
+            }
+        }).catch(err => {
+            expect(err.response.status).toEqual(500);
+            expect(err.response.data.message).toEqual('Timeout');
+            done();
+        });
+    });
+
+});
