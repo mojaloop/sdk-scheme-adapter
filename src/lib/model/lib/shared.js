dda28c9bd6f3dc809b647aa1d68e47f70c7c229d
Steven Oderayi <oderayi@gmail.com>
2021-07-28 10:40:01 +0100

    Update extensionlist

--- src/lib/model/lib/shared.js
+++ src/lib/model/lib/shared.js
@@ -250,8 +250,8 @@ const mojaloopPrepareToInternalTransfer = (external, quote) => {
             transactionType: quote.request.transactionType.scenario,
             note: quote.request.note
         };
-        if (quote.internalRequest && quote.internalRequest.extensionList && quote.internalRequest.extensionList.extension) {
-            internal.quoteRequestExtensions = { ...quote.internalRequest.extensionList.extension };
+        if (quote.internalRequest && quote.internalRequest.extensionList) {
+            internal.quoteRequestExtensions = { ...quote.internalRequest.extensionList };
         }
     } else {
         internal = {
