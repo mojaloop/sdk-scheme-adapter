5a48d9a4238965cc18d4765599ea241b9dcc0058
Vijay Kumar <vijaya.guthi@modusbox.com>
2022-07-15 11:44:22 +0530

    fix: some dependencies

--- modules/private-shared-lib/src/index.ts
+++ modules/private-shared-lib/src/index.ts
@@ -24,7 +24,7 @@
  * Modusbox
  - Shashikant Hirugade <shashikant.hirugade@modusbox.com>
  - Juan Correa <juancorrea@modusbox.com>
- - Vijaya Kumar Guthi <vijaya.guthi@modusbox.com>
+ - Vijay Kumar Guthi <vijaya.guthi@modusbox.com>
 
  --------------
  ******/
