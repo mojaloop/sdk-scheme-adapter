804fdb1cd76c3a734eef06892295f349adbec729
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2022-09-01 15:55:59 +0530

    Update modules/outbound-command-event-handler/src/domain/bulk_batch_entity.ts
    Co-authored-by: Miguel de Barros <miguel@debarros.me>

--- modules/outbound-command-event-handler/src/domain/bulk_batch_entity.ts
+++ modules/outbound-command-event-handler/src/domain/bulk_batch_entity.ts
@@ -57,7 +57,7 @@ export interface BulkBatchState extends BaseEntityState {
     bulkQuotesResponse?: SDKSchemeAdapter.Outbound.V2_0_0.Types.bulkQuoteResponse;
     bulkTransfersRequest: SDKSchemeAdapter.Outbound.V2_0_0.Types.bulkTransferRequest;
     bulkTransfersResponse?: SDKSchemeAdapter.Outbound.V2_0_0.Types.bulkQuoteResponse;
-    quoteIdReferenceIdMap: {[quoteId: string]: string};
+    quoteIdReferenceIdMap: {[quoteId: string]: string}; // Key = individual quoteId from request, Value = transactionId representing an individual-transfer
     transferIdReferenceIdMap: {[quoteId: string]: string};
     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
     lastError?: any; // TODO: Define a format for this
