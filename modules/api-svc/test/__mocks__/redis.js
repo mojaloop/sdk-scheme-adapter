3201a956ffc52d80835867b57f14ebcd976f3859
Kevin <kleyow@gmail.com>
2022-10-05 00:16:03 -0500

    chore: event emitter warning

--- modules/api-svc/test/__mocks__/redis.js
+++ modules/api-svc/test/__mocks__/redis.js
@@ -12,6 +12,7 @@ const redisMock = require('redis-mock');
 const { promisify } = require('util');
 
 const { EventEmitter } = require('events');
+EventEmitter.defaultMaxListeners = 50;
 
 const events = {};
 
