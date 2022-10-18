a8327f45bd6dc84a2ecee215f56031df6f9e8d07
Kevin <kleyow@gmail.com>
2022-10-18 00:32:32 -0500

    fix: added optional chaining to bulk transfer handler

--- modules/outbound-command-event-handler/src/domain/bulk_transaction_agg/handlers/process_bulk_transfers_callback.ts
+++ modules/outbound-command-event-handler/src/domain/bulk_transaction_agg/handlers/process_bulk_transfers_callback.ts
@@ -61,7 +61,7 @@ export async function handleProcessBulkTransfersCallbackCmdEvt(
 
         // If individual transfer result contains `lastError` the individual transfer state should be TRANSFER_FAILED.
         // bulkTransfersResult.currentState === 'ERROR_OCCURRED' necessitates erroring out all individual transfers in that bulk batch.
-        if(bulkTransfersResult.currentState === SDKOutboundTransferState.COMPLETED) {
+        if(bulkTransfersResult?.currentState === SDKOutboundTransferState.COMPLETED) {
             bulkBatch.setState(BulkBatchInternalState.TRANSFERS_COMPLETED);
             successCount = await bulkTransactionAgg.incrementBulkTransfersSuccessCount();
 
