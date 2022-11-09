78e9e5dc367e790e99fdf3f19e8823eaf8a5a675
Vijay <vijaya.guthi@modusbox.com>
2022-11-09 22:58:53 +0530

    chore: added some delay for testing

--- modules/outbound-command-event-handler/src/domain/bulk_transaction_agg/handlers/process_bulk_transfers_callback.ts
+++ modules/outbound-command-event-handler/src/domain/bulk_transaction_agg/handlers/process_bulk_transfers_callback.ts
@@ -36,6 +36,8 @@ import {
 import { BulkTransactionAgg } from '..';
 import { ICommandEventHandlerOptions } from '@module-types';
 import { BulkBatchInternalState, BulkTransactionInternalState, IndividualTransferInternalState } from '@mojaloop/sdk-scheme-adapter-private-shared-lib';
+// TODO: remove this
+import { setTimeout } from 'timers/promises';
 
 export async function handleProcessBulkTransfersCallbackCmdEvt(
     message: CommandEvent,
@@ -76,6 +78,8 @@ export async function handleProcessBulkTransfersCallbackCmdEvt(
                     });
                     individualTransfer.setTransactionId(bulkBatch.id);
                     await bulkTransactionAgg.setIndividualTransferById(individualTransfer.id, individualTransfer);
+                    // TODO: remove this
+                    await setTimeout(10, 'resolved');
                 } else {
                     const individualTransferId = bulkBatch.getReferenceIdForTransferId(transferResult.transferId);
                     const individualTransfer = await bulkTransactionAgg.getIndividualTransferById(individualTransferId);
