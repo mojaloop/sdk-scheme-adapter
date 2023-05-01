95b0c4a49a1a406ddebb2aad6698621d3d0b28e7
Vijay <vijaya.guthi@infitx.com>
2023-05-01 19:12:54 +0530

    fix: implemented put transactionrequest error handler

--- modules/api-svc/src/lib/model/OutboundRequestToPayModel.js
+++ modules/api-svc/src/lib/model/OutboundRequestToPayModel.js
@@ -270,13 +270,10 @@ class OutboundRequestToPayModel {
                     let error;
                     let message = JSON.parse(msg);
 
-                    // if (message.type === 'transactionRequestFail') {
-                    //     error = new BackendError(`Got an error response processing transaction request: ${util.inspect(message.data)}`, 500);
-                    //     error.mojaloopError = message.data;
-                    // } else {
-                    //     this._logger.push({ message }).log(`Ignoring cache notification for transfer ${transactionRequestKey}. Uknokwn message type ${message.type}.`);
-                    //     return;
-                    // }
+                    if (message.type === 'transactionRequestResponseError') {
+                        error = new BackendError(`Got an error response processing transaction request: ${util.inspect(message.data)}`, 500);
+                        error.mojaloopError = message.data;
+                    }
 
                     // cancel the timeout handler
                     clearTimeout(timeout);
@@ -292,8 +289,7 @@ class OutboundRequestToPayModel {
 
                     const transactionRequestResponse = message.data;
                     this._logger.push({ transactionRequestResponse }).log('Transaction Request Response received');
-                    this.data.requestToPayState = transactionRequestResponse.transactionRequestState;
-
+                    this.data.transactionRequestResponse = transactionRequestResponse;
 
                     return resolve(transactionRequestResponse);
                 }
@@ -450,15 +446,15 @@ class OutboundRequestToPayModel {
 
             // as this function is recursive, we dont want to error the state machine multiple times
             if(this.data.currentState !== 'errored') {
-                // err should not have a transferState property here!
-                if(err.transferState) {
+                // err should not have a requestToPayState property here!
+                if(err.requestToPayState) {
                     this._logger.log(`State machine is broken: ${util.inspect(err)}`);
                 }
                 // transition to errored state
                 await this.stateMachine.error(err);
 
                 // avoid circular ref between transferState.lastError and err
-                err.transferState = JSON.parse(JSON.stringify(this.getResponse()));
+                err.requestToPayState = JSON.parse(JSON.stringify(this.getResponse()));
             }
             throw err;
         }
