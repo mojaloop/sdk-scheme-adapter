d5fa071013580ae951a0c81968400b0131828322
vijayg10 <33152110+vijayg10@users.noreply.github.com>
2023-07-07 20:09:53 +0530

    feat(mojaloop/#3264): tls cert reload issue (#454)
    * fix: issue with tls cert reloading
    
    * chore: updated docker stack
    
    * feat: added pm4ml tests
    
    * fix: container names
    
    * chore: update deps
    
    * fix: integration tests
    
    * fix: cert generation
    
    * fix: tests
    
    * fix: issues
    
    * chore(snapshot): 23.1.0-snapshot.0

---
+++ docker/haproxy/tls/createSecrets.sh
@@ -0,0 +1,37 @@
+OUTPUT_DIR="."
+setopt +o nomatch
+rm *.key
+rm *.pem
+rm *.csr
+rm *.crt
+rm *.srl
+
+#####################
+
+## Generating Hub CA certificate
+openssl req -x509 -config openssl-hub-ca.cnf -newkey rsa:4096 -sha256 -nodes -out hub_cacert.pem -outform PEM
+
+## Generating DFSP CA certificate
+openssl req -x509 -config openssl-dfsp-ca.cnf -newkey rsa:4096 -sha256 -nodes -out dfsp_cacert.pem -outform PEM
+
+#####################
+
+## Generate Hub server csr
+openssl req -config openssl-hub-server.cnf -newkey rsa:4096 -sha256 -nodes -out hub_server.csr -outform PEM
+
+## Sign Hub server cert with DFSP CA
+openssl ca -batch -config openssl-dfsp-ca.cnf -policy signing_policy -extensions signing_req -out hub_server_cert.pem -infiles hub_server.csr
+
+## Append ca cert to PEM file
+cat dfsp_cacert.pem >> hub_server_cert.pem
+
+## Append key to PEM file
+cat hub_server_key.key >> hub_server_cert.pem
+
+#####################
+
+## Generate client csr
+openssl req -config openssl-dfsp-client.cnf -newkey rsa:4096 -sha256 -nodes -out dfsp_client.csr -outform PEM
+
+## Sign DFSP client cert with Hub CA
+openssl ca -batch -config openssl-hub-ca.cnf -policy signing_policy -extensions signing_req -out dfsp_client_cert.crt -infiles dfsp_client.csr
---
+++ modules/api-svc/test/integration-pm4ml/FSPIOPEventHandler/partyInfoRequest.test.js
@@ -0,0 +1,174 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+ * Modusbox
+ - Vijay Kumar Guthi <vijaya.guthi@infitx.com
+ --------------
+ ******/
+
+ "use strict";
+
+const { DefaultLogger } = require('@mojaloop/logging-bc-client-lib');
+const {
+  KafkaDomainEventConsumer,
+  KafkaDomainEventProducer,
+  PartyInfoRequestedDmEvt,
+} = require('@mojaloop/sdk-scheme-adapter-private-shared-lib');
+
+const { randomUUID } = require('crypto');
+const axios = require('axios');
+jest.unmock('@mojaloop/sdk-scheme-adapter-private-shared-lib');
+jest.unmock('@mojaloop/sdk-standard-components');
+jest.unmock('redis');
+jest.unmock('uuidv4');
+jest.unmock('javascript-state-machine');
+// Tests can timeout in a CI pipeline so giving it leeway
+jest.setTimeout(50000);
+
+const MANAGEMENT_MOCK_SERVER_URL = 'http://localhost:5005';
+const fs = require('fs');
+const TLS_FOLDER_PATH = '../../docker/haproxy/tls'
+const dfspCACert = fs.readFileSync(TLS_FOLDER_PATH + '/dfsp_cacert.pem', 'utf8');
+const clientCert = fs.readFileSync(TLS_FOLDER_PATH + '/dfsp_client_cert.crt', 'utf8');
+const clientKey = fs.readFileSync(TLS_FOLDER_PATH + '/dfsp_client_key.key', 'utf8');
+
+const logger = new DefaultLogger('bc', 'appName', 'appVersion'); //TODO: parameterize the names here
+
+// Setup for Kafka Producer
+const domainEventProducerOptions = {
+  brokerList: 'localhost:9092',
+  clientId: 'test-integration-client-id',
+  topic: 'topic-sdk-outbound-domain-events'
+}
+const producer = new KafkaDomainEventProducer(domainEventProducerOptions, logger)
+
+// Setup for Kafka Consumer
+const domainEventConsumerOptions = {
+  brokerList: 'localhost:9092',
+  clientId: 'test-integration-client-id2',
+  topics: ['topic-sdk-outbound-domain-events'],
+  groupId: "test-integration-group-id"
+}
+var domainEvents = []
+const _messageHandler = async (message) => {
+  domainEvents.push(message);
+}
+const consumer = new KafkaDomainEventConsumer(_messageHandler.bind(this), domainEventConsumerOptions, logger)
+
+describe("Tests for discovery part in FSPIOP Handler", () => {
+
+  beforeEach(async () => {
+    domainEvents = [];
+  });
+
+  beforeAll(async () => {
+    await producer.init();
+    await consumer.init();
+    await consumer.start();
+  });
+
+  afterAll(async () => {
+    await producer.destroy();
+    await consumer.destroy();
+  });
+
+  // TESTS FOR PARTY LOOKUP
+  test.only("partyInfoRequestedDmEvt should result in failure due to lack of outbound TLS certificates", async () => {
+    // Clear the existing configuration if any
+    await axios.post(`${MANAGEMENT_MOCK_SERVER_URL}/resetConfig`);
+
+    const partyInfoRequestedDmEvt = new PartyInfoRequestedDmEvt({
+    bulkId: randomUUID(),
+    content: {
+        transferId: randomUUID(),
+        request: {
+            partyIdType: 'MSISDN',
+            partyIdentifier: '1234567890',
+            partySubIdOrType: null,
+        },
+    },
+    timestamp: Date.now(),
+    headers: [],
+    });
+
+    // Wait for the TLS configuration change to be propagated to SDK
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    await producer.sendDomainEvent(partyInfoRequestedDmEvt);
+
+    // TODO: The following timeout can be enhanced with wrapRetries [https://github.com/mojaloop/project/issues/3408]
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    // Check domain events published to kafka
+    expect(domainEvents.length).toEqual(2);
+    expect(domainEvents[0].getName()).toBe('PartyInfoRequestedDmEvt');
+    expect(domainEvents[1].getName()).toBe('PartyInfoCallbackReceivedDmEvt');
+    const callbackContent = domainEvents[1].getContent();
+    // TODO: We are not checking for the specific error because SDK error response doesn't contain helpful error codes.
+    // There is a story to address this issue [https://github.com/mojaloop/project/issues/3407]
+    expect(callbackContent).toHaveProperty('partyErrorResult');
+
+  });
+  test.only("partyInfoRequestedDmEvt should pass after updating outbound TLS certificates", async () => {
+    // Now update the outbound TLS configuration
+    await axios.post(`${MANAGEMENT_MOCK_SERVER_URL}/updateOutboundTLSConfig`, {
+      "mutualTLS": {
+          "enabled": true
+      },
+      "creds": {
+          "ca": dfspCACert,
+          "cert": clientCert,
+          "key": clientKey
+      }
+    });
+
+    const partyInfoRequestedDmEvt = new PartyInfoRequestedDmEvt({
+    bulkId: randomUUID(),
+    content: {
+        transferId: randomUUID(),
+        request: {
+            partyIdType: 'MSISDN',
+            partyIdentifier: '1234567890',
+            partySubIdOrType: null,
+        },
+    },
+    timestamp: Date.now(),
+    headers: [],
+    });
+
+    // Wait for the TLS configuration change to be propagated to SDK
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    await producer.sendDomainEvent(partyInfoRequestedDmEvt);
+
+    // TODO: The following timeout can be enhanced with wrapRetries [https://github.com/mojaloop/project/issues/3408]
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    // Check domain events published to kafka
+    expect(domainEvents.length).toEqual(2);
+    expect(domainEvents[0].getName()).toBe('PartyInfoRequestedDmEvt');
+    expect(domainEvents[1].getName()).toBe('PartyInfoCallbackReceivedDmEvt');
+    const callbackContent = domainEvents[1].getContent();
+    expect(callbackContent).toHaveProperty('partyResult');
+    expect(callbackContent.partyResult).toHaveProperty('currentState');
+    expect(callbackContent.partyResult.currentState).toEqual('COMPLETED');
+
+
+  });
+});
