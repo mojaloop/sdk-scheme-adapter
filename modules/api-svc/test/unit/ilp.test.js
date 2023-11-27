e7e89ea3dae06f41e0213f19383b58930de9ee61
geka.evk <geka.evk@gmail.com>
2023-11-27 17:39:30 +0000

    feat(3618): added ilp unit-test;  updated Ilp version

---
+++ modules/api-svc/test/unit/ilp.test.js
@@ -0,0 +1,46 @@
+const { Ilp } = jest.requireActual('@mojaloop/sdk-standard-components');
+const mocks = require('./lib/model/data/mocks');
+
+describe('ILP Tests -->', () => {
+    let ilp;
+
+    beforeEach(() => {
+        ilp = new Ilp({
+            secret: 'test',
+            logger: { log: jest.fn() }
+        });
+    });
+
+    test('should generate ILP components for a fxQuote request', () => {
+        const fxQuotesPayload = mocks.mockFxQuotesPayload();
+        const fxpBeResponse = mocks.mockFxQuotesInternalResponse();
+
+        const {
+            ilpPacket,
+            fulfilment,
+            condition
+        } = ilp.getFxQuoteResponseIlp(fxQuotesPayload, fxpBeResponse);
+
+        expect(ilpPacket).toBeTruthy();
+        expect(fulfilment).toBeTruthy();
+        expect(condition).toBeTruthy();
+
+        const decodedIlp = ilp.decodeIlpPacket(ilpPacket);
+        expect(decodedIlp).toBeTruthy();
+        expect(decodedIlp.amount).toBe('0');
+        expect(decodedIlp.sourceAmount).toBeUndefined();
+        expect(decodedIlp.targetAmount).toBeUndefined();
+        expect(typeof decodedIlp.account).toBe('string');
+        expect(decodedIlp.data).toBeInstanceOf(Buffer);
+
+        const decodedJson = JSON.parse(Buffer.from(decodedIlp.data.toString(), 'base64').toString());
+        const { conversionRequestId } = fxQuotesPayload;
+        const { conversionTerms } = fxpBeResponse;
+        const transactionObject = {
+            conversionRequestId,
+            conversionTerms
+        };
+        expect(decodedJson).toEqual(transactionObject);
+    });
+});
+
