0db290430aa9720a85d106b83fdd005eb04ee8d5
geka.evk <geka.evk@gmail.com>
2024-09-06 13:53:14 +0100

    feat(csi-194): updated ILP packet to v4

--- modules/api-svc/test/unit/ilp.test.js
+++ modules/api-svc/test/unit/ilp.test.js
@@ -1,20 +1,21 @@
-const { Ilp } = jest.requireActual('@mojaloop/sdk-standard-components');
+const { Ilp, Logger } = jest.requireActual('@mojaloop/sdk-standard-components');
 const mocks = require('./lib/model/data/mocks');
 
 describe('ILP Tests -->', () => {
     let ilp;
+    let fxQuotesPayload;
+    let fxpBeResponse;
 
     beforeEach(() => {
         ilp = new Ilp({
             secret: 'test',
-            logger: { log: jest.fn() }
+            logger: new Logger.Logger(),
         });
+        fxQuotesPayload = mocks.mockFxQuotesPayload();
+        fxpBeResponse = mocks.mockFxQuotesInternalResponse();
     });
 
-    test('should generate ILP components for a fxQuote request', () => {
-        const fxQuotesPayload = mocks.mockFxQuotesPayload();
-        const fxpBeResponse = mocks.mockFxQuotesInternalResponse();
-
+    test('should generate ILP response based on fxQuotes request/response', () => {
         const {
             ilpPacket,
             fulfilment,
@@ -24,12 +25,18 @@ describe('ILP Tests -->', () => {
         expect(ilpPacket).toBeTruthy();
         expect(fulfilment).toBeTruthy();
         expect(condition).toBeTruthy();
+    });
+
+    test('should generate proper ILP packet for fxQuote', () => {
+        const { ilpPacket } = ilp.getFxQuoteResponseIlp(fxQuotesPayload, fxpBeResponse);
 
         const decodedIlp = ilp.decodeIlpPacket(ilpPacket);
         expect(decodedIlp).toBeTruthy();
         expect(typeof decodedIlp.amount).toBe('string');
-        expect(typeof decodedIlp.account).toBe('string');
+        expect(typeof decodedIlp.destination).toBe('string');
         expect(decodedIlp.data).toBeInstanceOf(Buffer);
+        expect(decodedIlp.expiresAt).toBeInstanceOf(Date);
+        expect(decodedIlp.executionCondition).toBeInstanceOf(Buffer);
 
         const decodedJson = JSON.parse(Buffer.from(decodedIlp.data.toString(), 'base64').toString());
         const { conversionRequestId } = fxQuotesPayload;
@@ -39,6 +46,13 @@ describe('ILP Tests -->', () => {
             conversionTerms
         };
         expect(decodedJson).toEqual(transactionObject);
+    });
+
+    test('should generate ILP packet with proper condition', () => {
+        const { ilpPacket, condition } = ilp.getFxQuoteResponseIlp(fxQuotesPayload, fxpBeResponse);
+        const { executionCondition } = ilp.decodeIlpPacket(ilpPacket);
+        expect(executionCondition).toBeInstanceOf(Buffer);
+        expect(condition).toBe(executionCondition.toString('base64url'));
     });
 });
 
