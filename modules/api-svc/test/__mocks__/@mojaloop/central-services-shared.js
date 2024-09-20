12e864d24d00f4653061e4280c6c33ccfb79d5c3
Kalin Krustev <kalin.krustev@gmail.com>
2024-09-20 07:10:14 +0000

    test: fix unit and functional tests

--- modules/api-svc/test/__mocks__/@mojaloop/central-services-shared.js
+++ modules/api-svc/test/__mocks__/@mojaloop/central-services-shared.js
@@ -1,9 +1,6 @@
 const components = jest.requireActual('@mojaloop/central-services-shared');
-const { Util: {id: idGenerator} } = require('@mojaloop/central-services-shared');
 
-let id = idGenerator({ type: 'ulid' });
+let id = 0;
 
-components.Util.id = () => () => id(1);
-components.Util.id.__reset = () => { idGenerator({ type: 'ulid' }); };
-
-module.exports = components;
+components.Util.id = () => () => (++id).toString().padStart(26, '0');
+components.Util.id.__reset = () => { id = 0; };
