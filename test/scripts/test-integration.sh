6d4deb1764108af4a12ee7949b5b3cdf4d752214
Kalin Krustev <kalin.krustev@gmail.com>
2024-11-04 10:17:34 +0000

    chore: build

---
+++ test/scripts/test-integration.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -xe
+
+echo "Validating OpenAPI specs"
+yarn run build:openapi && yarn run validate:api
+
+run_int_tests() {
+  pushd modules/$1
+  yarn run start & echo $! > /tmp/sdk-scheme-adapter.pid
+  sleep 10
+  yarn run test:integration
+  kill $(cat /tmp/sdk-scheme-adapter.pid)
+  popd
+}
+
+echo "Running outbound-domain integration tests"
+run_int_tests outbound-domain-event-handler
+
+echo "Running outbound-command integration tests"
+run_int_tests outbound-command-event-handler
+
+echo "Execute PM4ML Integration Tests"
+pushd modules/api-sv
+yarn run test:integration-pm4ml
+popd
