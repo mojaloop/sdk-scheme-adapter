39b7c476df6959c41137f88c9e11e0b36b2d37c7
geka.evk <geka.evk@gmail.com>
2024-11-13 19:36:12 +0000

    feat(csi-128): skipped test coverage check for outbound handlers

--- modules/outbound-command-event-handler/jest.config.js
+++ modules/outbound-command-event-handler/jest.config.js
@@ -18,13 +18,13 @@ module.exports = {
   ],
   coverageReporters: ["json", "lcov"],
   clearMocks: true,
-  // coverageThreshold: {
-  //   "global": {
-  //     "branches": 75,
-  //     "functions": 80,
-  //     "lines": 80,
-  //     "statements": -10
-  //   }
-  // },
+  coverageThreshold: {
+    "global": {
+      "branches": 75,
+      "functions": 80,
+      "lines": 80,
+      "statements": -10
+    }
+  },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 }
