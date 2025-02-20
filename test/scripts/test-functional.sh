6a7c4a2758fcd1a0e21876c3a5f906dd8641e7b9
Vijay <vijaya.guthi@infitx.com>
2025-02-21 01:31:04 +0530

    chore: deps

--- test/scripts/test-functional.sh
+++ test/scripts/test-functional.sh
@@ -5,10 +5,12 @@ cd test/func_bulk
 docker compose --profile bulk up -d
 sleep 10
 docker compose -f ./ttk-tests-docker-compose.yml up --abort-on-container-exit
+docker compose -f ./ttk-tests-docker-compose.yml down -v
 docker compose --profile bulk down -v
 
 cd ../../test/func_iso20022
 docker compose up -d
-sleep 10
+sleep 30
 docker compose -f ./ttk-tests-docker-compose.yml up --abort-on-container-exit
+docker compose -f ./ttk-tests-docker-compose.yml down -v
 docker compose down -v
