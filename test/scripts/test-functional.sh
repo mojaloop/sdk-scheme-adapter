3fc02904f6b72a18000e7e1f3ebf2e1444daa50a
Vijay <vijaya.guthi@infitx.com>
2025-02-21 00:38:30 +0530

    chore: fix func tests

--- test/scripts/test-functional.sh
+++ test/scripts/test-functional.sh
@@ -5,10 +5,10 @@ cd test/func_bulk
 docker compose --profile bulk up -d
 sleep 10
 docker compose -f ./ttk-tests-docker-compose.yml up --abort-on-container-exit
-docker compose --profile bulk down
+docker compose --profile bulk down -v
 
 cd ../../test/func_iso20022
 docker compose up -d
 sleep 10
 docker compose -f ./ttk-tests-docker-compose.yml up --abort-on-container-exit
-docker compose down
+docker compose down -v
