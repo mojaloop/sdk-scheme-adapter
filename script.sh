8cf37d5379b650208459b408f00f939ca1a1eef8
devarsh10 <devarshshah2608@gmail.com>
2025-12-04 14:32:02 +0000

    remove script file

--- script.sh
+++
@@ -1,9 +0,0 @@
-echo "Running integration tests...."
-if [ -f yarn.lock ]; then
-  yarn install --immutable
-  yarn run build
-  yarn run test:integration
-else
-  npm ci
-  npm run test:integration
-fi
