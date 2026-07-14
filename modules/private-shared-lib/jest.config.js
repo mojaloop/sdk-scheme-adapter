85e148491cc99f60a9fca3822b8753737594ba5f
ths1959 <ths2891503@gmail.com>
2026-07-14 13:21:49 +0800

    fix: resolve Jest configuration issues for private-shared-lib

--- modules/private-shared-lib/jest.config.js
+++ modules/private-shared-lib/jest.config.js
@@ -3,24 +3,13 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverage: false,
-  collectCoverageFrom: ["./src/**/*.ts"],
-  coverageReporters: ["json", "lcov"],
   clearMocks: true,
-  coverageThreshold: {
-    "global": {
-      "branches": 0,
-      "functions": 0,
-      "lines": 0,
-      "statements": 0
-    }
-  },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
     "^.+\\.(js|jsx|mjs)$": "ts-jest"
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!serialize-error|non-error|@mojaloop/central-services-shared|@mojaloop/event-sdk|uuid).+\\.js$"
-  ],
-  forceExit: true
+    "/node_modules/(?!serialize-error|non-error|@mojaloop/central-services-shared|@mojaloop/event-sdk|uuid).+\\.js$",
+    "/dist/"
+  ]
 }
